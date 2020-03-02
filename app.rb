278788f28f7351cffb6897c980b3a231be4eafed
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-02 15:43:13 +0000

    params

--- app.rb
+++ app.rb
@@ -13,7 +13,13 @@ get "/thirdcheck" do
 "has this really really updated?"
 end
 
-get "/cat" do
+get "/random-cat" do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get "/named-cat" do
+  p params[:name]
+  @name = params[:name]
   erb(:index)
 end
