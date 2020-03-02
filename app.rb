f6a231c7a48409a0f5c5c3432d6cd7c9e12ae518
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-02 15:29:02 +0000

    Title with interpolated string

--- app.rb
+++ app.rb
@@ -13,8 +13,6 @@ get "/thirdcheck" do
 "has this really really updated?"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-   </div>"
+get "/cat" do
+  erb(:index)
 end
